767b911e2d5c1356530ffb292b21202f57943434
renanpd95 <renandomiiciano95@gmail.com>
2023-12-05 15:12:31 -0300

    Mensagem do commit

--- app.js
+++ app.js
@@ -44,70 +44,40 @@ app.post('/cadastrarDados', (req, res) => {
   );
 });
 
-// Rota para inserir um profissional
+
+// Rota para receber os dados do formulário
 app.post('/profissionais', (req, res) => {
-  const {
-    usuario,
-    senha,
-    servico_prestado,
-    observacoes,
-    local_atendimento,
-    segunda,
-    segunda_inicio,
-    segunda_fim,
-    // adicione outros campos aqui...
-  } = req.body;
+  const data = req.body;
 
-  const inserirProfissionalQuery = `
-    INSERT INTO profissionais (
-      usuario,
-      senha,
-      servico_prestado,
-      observacoes,
-      local_atendimento,
-      segunda,
-      segunda_inicio,
-      segunda_fim
-      -- adicione outros campos aqui...
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-  `;
-
-  db.query(
-    inserirProfissionalQuery,
-    [
-      usuario,
-      senha,
-      servico_prestado,
-      observacoes,
-      local_atendimento,
-      segunda,
-      segunda_inicio,
-      segunda_fim,
-      // adicione outros valores aqui...
-    ],
-    (err, result) => {
-      if (err) {
-        console.error('Erro ao inserir profissional:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' });
-      } else {
-        console.log('Profissional inserido com sucesso!');
-        res.status(201).json({ success: true, id: result.insertId });
+    connection.query(
+      'INSERT INTO profissionais (usuario, senha, servico_prestado, observacoes, local_atendimento, segunda,segunda_inicio, segunda_fim, terca, terca_inicio, terca_fim, quarta, quarta_inicio, quarta_fim, quinta, quinta_inicio, quinta_fim, sexta, sexta_inicio, sexta_fim, sabado, sabado_inicio, sabado_fim, domingo,domingo_inicio, domingo_fim) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [data.usuario, data.senha, data.servico, data.observacoes, data.local, data.segundaChecked, data.segundaHoraInicio, data.segundaHoraFim, data.tercaChecked, data.tercaHoraInicio, data.tercaHoraFim, data.quartaChecked, data.quartaHoraInicio, data.quartaHoraFim, data.quintaChecked, data.quintaHoraInicio, data.quintaHoraFim, data.sextaChecked, data.sextaHoraInicio, data.sextaHoraFim, data.sabadoChecked, data.sabadoHoraInicio, data.sabadoHoraFim, data.domingoChecked, data.domingoHoraInicio, data.domingoHoraFim],
+      (error, results) => {
+        if (error) {
+          console.error('Erro ao inserir dados:', error);
+          res.status(500).json({ error: 'Erro ao cadastrar dados' });
+          return;
+        }
+  
+        res.status(200).json({ success: true, message: 'Cadastro realizado com sucesso' });
       }
+    );
+  });
+
+  
+// Rota para obter dados de todos os profissionais
+app.get('/profissionais', (req, res) => {
+  const query = 'SELECT * FROM sua_tabela';
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Erro ao executar a consulta:', error);
+      res.status(500).send('Erro ao processar a requisição.');
+      return;
     }
-  );
+
+    res.status(200).json(results);
+  });
 });
-
-// Endpoint para obter horários disponíveis
-app.get("/api/disponibilidade", (req, res) => {
-  const selectedDay = req.query.dia; // Obtém o dia da consulta de consulta
-
-  // Aqui você deve consultar seu banco de dados para obter os horários disponíveis
-  // com base no dia selecionado e enviar a resposta como um array de horários.
-  // Por enquanto, vamos apenas enviar alguns dados fictícios.
-  const disponibilidade = ["09:00", "10:00", "12:00"];
-  res.json(disponibilidade);
-});
-
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
